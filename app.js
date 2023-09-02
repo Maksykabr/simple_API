d63d1e1b4f3755c8f43eb92ee7ffda2bde3a1c55
Maksykabr <95287886+Maksykabr@users.noreply.github.com>
2023-09-02 13:31:54 +0300

    ninth_commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jayson = require('jayson');
 const fs = require('fs');
+const { error } = require('console');
 
 const filePath = './data/data.json'
 
@@ -12,9 +13,9 @@ const all_data = {
         console.log(error);
       }
       console.log('ok1');
-      const items = JSON.parse(data);
-      console.log(items)
-      callback(null, `${items}`)
+      // const items = JSON.parse(data);
+      console.log(data)
+      callback(null, `${data}`)
     });
   }
 };
@@ -31,12 +32,98 @@ const get_firstLevel = {
       const first_id = params.first_id;
       const foundObject = items.find((item) => item['Перший рівень'] == first_id);
       if (foundObject){
-        console.log(foundObject)
-        callback(null, `${foundObject}`)
+        foundedObject = JSON.stringify(foundObject)
+        console.log(foundedObject)
+        callback(null, `${foundedObject}`)
       } else {
         callback(null, 'Object not found');
       }
 
+    });
+  }
+};
+
+
+const get_secondLevel = {
+  get_secondObjects: function(params, callback) {
+    fs.readFile(filePath, 'utf8', (error, data) =>{
+      if (error) {
+        console.log(error);
+      }
+      console.log('ok1');
+      const items = JSON.parse(data);
+      const first_id = params.first_id;
+      const second_id = params.second_id;
+      const firstObjects = items.find((item) => item['Перший рівень'] == first_id);
+      const first_preparedObjects = firstObjects['Масив елементів другого рівня'];
+      const secondObjects = first_preparedObjects.find((object) => object['Другий рівень'] == second_id);
+      if (secondObjects) {
+        foundedObjects = JSON.stringify(secondObjects)
+        console.log(foundedObjects)
+        callback(null, `${foundedObjects}`)
+      } else {
+        callback(null, 'Object not found');
+      }
+    })
+  }
+}
+
+
+const get_thirdLevel = {
+  get_thirdObjects: function(params, callback) {
+    fs.readFile(filePath, 'utf8', (error, data) =>{
+      if (error) {
+        console.log(error);
+      }
+      console.log('ok1');
+      const items = JSON.parse(data);
+      const first_id = params.first_id;
+      const second_id = params.second_id;
+      const third_id = params.third_id;
+      const firstObjects = items.find((item) => item['Перший рівень'] == first_id);
+      const first_preparedObjects = firstObjects['Масив елементів другого рівня'];
+      const secondObjects = first_preparedObjects.find((object) => object['Другий рівень'] == second_id);
+      const second_preparedObjects = secondObjects['Масив елементів третього рівня'];
+      const thirdObjects = second_preparedObjects.find((object) => object['Третій рівень'] == third_id);
+      if (thirdObjects) {
+        foundedObjects = JSON.stringify(thirdObjects);
+        console.log(foundedObjects);
+        callback(null, `${foundedObjects}`);
+      } else {
+        callback(null, 'Object not found');
+      }
+    });
+  }
+};
+
+
+const get_fourthLevel = {
+  get_fourthObjects: function(params, callback){
+    fs.readFile(filePath, 'utf8', (error, data) =>{
+      if (error) {
+        console.log(error);
+      }
+      console.log('ok1');
+      const items = JSON.parse(data);
+      const first_id = params.first_id;
+      const second_id = params.second_id;
+      const third_id = params.third_id;
+      const fourth_id = params.fourth_id;
+
+      const firstObjects = items.find((item) => item['Перший рівень'] == first_id);
+      const first_preparedObjects = firstObjects['Масив елементів другого рівня'];
+      const secondObjects = first_preparedObjects.find((object) => object['Другий рівень'] == second_id);
+      const second_preparedObjects = secondObjects['Масив елементів третього рівня'];
+      const thirdObjects = second_preparedObjects.find((object) => object['Третій рівень'] == third_id);
+      const thid_preparedObjects = thirdObjects['Масив елементів четвертого рівня'];
+      const fourthObjects = thid_preparedObjects.find((object) => object['Четвертий рівень'] == fourth_id);
+      if (fourthObjects) {
+        foundedObjects = JSON.stringify(fourthObjects);
+        console.log(foundedObjects);
+        callback(null, `${foundedObjects}`);
+      } else {
+        callback(null, 'Object not found');
+      };
     });
   }
 };
@@ -55,8 +142,13 @@ const dogs = {
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+// get
 app.post('/get_data', jayson.server(all_data).middleware());
 app.post('/get_firstLevel', jayson.server(get_firstLevel).middleware());
+app.post('/get_secondLevel', jayson.server(get_secondLevel).middleware());
+app.post('/get_thirdLevel', jayson.server(get_thirdLevel).middleware());
+app.post('/get_fourthLevel', jayson.server(get_fourthLevel).middleware())
+// post
 
 app.post('/cats', jayson.server(cats).middleware());
 app.post('/dogs', jayson.server(dogs).middleware());
